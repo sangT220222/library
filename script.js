6bba3d4fce2fcb4ac6fa5650e6f166d51a637f5b
sangT220222 <magnas9@hotmail.co.uk>
2024-02-29 22:31:45 +0000

    added logic for books display so that it updates whenver user adds a book, dialog box is added for better user interfaace

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
-const my_library = [
-//default books in library
-{name: "Book1", author:"John Smith", pages: 69, status: "Read"},
-{name: "Book2", author:"Ben Dover", pages: 169, status: "Not Read"},
-{name: "Book3", author:"Mike Lee", pages: 269, status: "Not Read"},
-]; //giving library default values for testing purposes
+var my_library = []; //intialising array
 
 function Book(name,author,pages,status) {
   //constructor
@@ -13,53 +8,63 @@ function Book(name,author,pages,status) {
   this.status = status;
 }
 
-function addBookToLibrary(book){
-  my_library.push(book); //appending book to the end of my_library array
-}
+
+const add_book = document.createElement("button"); //creating html tags
+add_book.textContent = "Add Book";
 
 //printing books out in the libary
 function displayBooks(){
+  while(document.getElementById("books_container")){ //this loop checks if books_container exists in the html or not
+    var to_remove = document.getElementById("books_container"); 
+    to_remove.parentNode.removeChild(to_remove);//removes the element stored inside to_remove from its parent_node, this case it removes the books_container from its parent node - body
+  }
+
+  const container = document.createElement("div"); //creating a new div 
+  container.setAttribute("id","books_container"); 
+  document.body.appendChild(container);
+  //below is a loop that will go through the items in my_library array, and then append the items onto the html page based on instructions set
   my_library.forEach(function(book){
-    const card = document.createElement("div");//creating a new div 
+    const card = document.createElement("div");
     card.innerHTML = `
     <h3>${book.name}</h3> 
     <h4>${book.author}</h4>
     <h6>${book.pages}</h6>
     <p>${book.status}</p>
     `; //setting the HTML layout up within the specified tag
-    card_container.appendChild(card); //appending the newly created tag and content, inside card_container
+    container.appendChild(card); //appending the newly created tag and content, inside card_container
   })
 }
 
-displayBooks();
-const add_book = document.createElement("button"); //creating html tags
-add_book.textContent = "Add Book";
 
-const book_form = document.createElement("form");
+const book_form = document.createElement("dialog");
 book_form.innerHTML =   ` 
-  <label for="book_name">Book name:</label>
-  <input type="text" id="book_name" name="book_name">
-  <label for="author">Author:</label>
-  <input type="text" id="author" name="author">
-  <label for="pages">Pages:</label>
-  <input type="text" id="pages" name="pages">
-  <label for="status">Status:</label>
-  <input type="text" id="status" name="status">
-  <button type = "submit">Submit</button>
+  <form method = "dialog">
+    <label for="book_name">Book name:</label>
+    <input type="text" id="book_name" name="book_name">
+    <label for="author">Author:</label>
+    <input type="text" id="author" name="author">
+    <label for="pages">Pages:</label>
+    <input type="text" id="pages" name="pages">
+    <label for="status">Status:</label>
+    <input type="text" id="status" name="status">
+    <button type = "submit">Submit</button>
+  </form>
 ` //pure replica of what you would type in a html file
 
 document.body.appendChild(add_book);
 document.body.appendChild(book_form);
 //append to body tag in html
 
-book_form.style.display = "none"; //hiding the book form initially
+// book_form.style.display = "none"; //hiding the book form initially
 
 add_book.addEventListener("click", function(){
-  book_form.style.display = "block"; //visibility is changed when add_book btn is clicked
+  // book_form.style.display = "block"; //visibility is changed when add_book btn is clicked
+  book_form.showModal();
+
 })
 
 book_form.addEventListener("submit", function(event){
-  event.preventDefault() // this will prevent the input to send the data to a server 
+  event.preventDefault(); // this will prevent the input to send the data to a server 
 
   const book_name = document.getElementById("book_name");
   const author = document.getElementById("author");
@@ -67,11 +72,15 @@ book_form.addEventListener("submit", function(event){
   const status = document.getElementById("status");
 
   //obtaining value that are in the input tag - specified above, and creating a new Book object
-  const new_book = new Book(book_name.value,author.value,pages.value,status.value);
+  var new_book = new Book(book_name.value,author.value,pages.value,status.value);
   
 
-  addBookToLibrary(new_book);//calling function to append new book onto end of my_library
-  console.log(my_library);
+  // addBookToLibrary(new_book);//calling function to append new book onto end of my_library
+  my_library.push(new_book); //appending created book into the end of my_library array
+  book_form.close();
+  displayBooks(); //adding book(s) inside my_library to the html page
 })
+
+
 
 
